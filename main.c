51948709a72a3bc4f23531c3a4032a39ca18ab4d
amitdavidi <amitdavidi12@gmail.com>
2022-11-20 12:26:25 +0200

    added error prints, support for error printing, errno

--- main.c
+++ main.c
@@ -4,7 +4,9 @@
 #include <string.h>
 #include <sys/wait.h>
 #include <sys/mman.h>
+ #include <errno.h>
 
+extern int errno; 
 
 
 #define PRINT_PROMPT printf("hw1shell$ ")
@@ -15,7 +17,9 @@
 #define MAX_LEN_OF_USER_COMMAND 100
 #define CHDIR_ERROR_CODE -1
 #define CHILD_PROCESS_FORK_RETURN_VALUE 0
-
+#define CHILD_PROCESS_ERROR_CODE -1
+#define SYSTEM_CALL_ERROR -1
+#define NO_ACESS_CODE -1
 
 int compare_last_element_in_string_and_remove(char *string_to_check, char character_to_compare) 
 {	
@@ -37,6 +41,7 @@ int main()
 
 	int error_flag = 0, running = 1;
 	int is_background_process = 0;
+	int system_call_status;
 
 	char command[MAX_CMD_LEN] = { 0 }; // user input
 	char *arguments[MAX_ARGV]; // parsed user input
@@ -62,69 +67,103 @@ int main()
 		PRINT_PROMPT;
 		fgets(command, sizeof(command), stdin);
 
+		/* user input parsing*/
+
 		if(command[0] == '\n') {
 			continue;
 		}
 
 		command[strcspn(command, "\n")] = 0; // remove new line character
-		/* Background if  command ends with & */
+
 		is_background_process = compare_last_element_in_string_and_remove(command, '&');
 
+		/* Parse arguments */
 		argc = 0;
 		char *arguments[MAX_ARGV];
-
-		/* Get the first token (cmd name) */
 		arguments[argc] = strtok(command, " ");
+		while((arguments[argc] != NULL) && (argc < MAX_ARGV)) {
+			arguments[++argc] = strtok(NULL, " "); 
+		}
 
-		/* Walk through the other tokens (parameters) */
-		while((arguments[argc] != NULL) && (argc < MAX_ARGV))
-			arguments[++argc] = strtok(NULL, " ");
+
+		/* execute command & arguments */
 
 		if(is_background_process) {
+			
+			/* max proccesses reached */
 			if(*number_of_active_processes == MAX_PROCESSES) {
 				fprintf(stderr, "hw1shell: too many background commands running\n");
 			}
 
+			/* new process */
 			else {
-				// create a background process
+				/*store command for future use - jobs*/
+				strcpy(child_process_command_history[*number_of_active_processes], command); 
+
 				pid_t fork_value = fork();
-				strcpy(child_process_command_history[*number_of_active_processes], command); // save the command
-			
-				if (fork_value == CHILD_PROCESS_FORK_RETURN_VALUE) {
-					pid_t current_pid = getpid(); 
 
-					for(int i = 0; i < MAX_PROCESSES; i++) {
-						if(pid_array_shared_memory[i] == 0) {
-							pid_array_shared_memory[i] = current_pid;
-							break;
+				/* fork failed - error 13.*/
+				if(fork_value == -1) { 
+					fprintf(stderr,"%s failed, errno is %d\n", arguments[0], errno);
+				}
+
+
+				/* fork successfull - do work */
+				else { 
+
+					// child process
+					if (fork_value == CHILD_PROCESS_FORK_RETURN_VALUE) {
+						pid_t current_pid = getpid();
+
+						/* insert into shared memory array */ 
+						for(int i = 0; i < MAX_PROCESSES; i++) { 
+							if(pid_array_shared_memory[i] == 0) {
+								pid_array_shared_memory[i] = current_pid;
+								(*number_of_active_processes)++;
+								break;
+							}
 						}
+
+						printf("pid %d started\n", current_pid);
+						system_call_status = execvp(arguments[0], arguments);
+						
+						/* system call fail + command fail*/
+						if(system_call_status == SYSTEM_CALL_ERROR) {
+							fprintf(stderr, "invalid command\n");
+							fprintf(stderr,"%s failed, errno is %d\n", arguments[0], errno);
+						}
+						
+						exit(0); // terminate process
 					}
-					(*number_of_active_processes)++;
-					
-					execvp(arguments[0], arguments);
 
+					// father process
+					else {
+						sleep(0.01);
+					}
 				}
-				else {
-					sleep(0.01);
-				}
-
 			}
 		}
 	
 		else if(strcmp(arguments[0], "cd") == 0) {
-			int chdir_result = chdir(arguments[1]);
-			if(chdir_result == CHDIR_ERROR_CODE)  {
+			
+			/* can access file ? */
+			if (access(arguments[1], F_OK) == NO_ACESS_CODE) {
 				fprintf(stderr, "hw1shell: invalid command\n");
 			}
+
+			/* file does exist - use chdir */
+			else {
+				int chdir_result = chdir(arguments[1]);
+				/* chdir failed - system call fail - error 13.*/
+				if(chdir_result == CHDIR_ERROR_CODE)  {
+					fprintf(stderr,"%s failed, errno is %d\n", arguments[0], errno);
+				}
+			}
 		}
-
+ 
 		else if(strcmp(arguments[0], "exit") == 0) {
-			
-			// kill procesess running in background..
-			// free dynamically allocated memory
 			running = 0;
-
-			wait(NULL); // reap child processes
+			wait(NULL); // reap all child processes
 		}
 
 		else if(strcmp(arguments[0], "jobs") == 0) {
@@ -136,35 +175,63 @@ int main()
 		}
 
 		else {
-
 			pid_t fork_value = fork();
 
-			if (fork_value == CHILD_PROCESS_FORK_RETURN_VALUE) {
-				*PID_external_command = getpid();
-				execvp(arguments[0], arguments);
+			/*fork failed - print error 13.*/
+			if(fork_value == -1) {
+					fprintf(stderr,"%s failed, errno is %d\n", arguments[0], errno);
+				}
 
-				exit(0);
-			}
+			/* fork successfull - do work */
 			else {
-				sleep(0.01);
-				waitpid(*PID_external_command, (int*)NULL, 0);
+				if (fork_value == CHILD_PROCESS_FORK_RETURN_VALUE) {
+					*PID_external_command = getpid();
+
+					system_call_status = execvp(arguments[0], arguments);
+					
+					/* system call failed + command fail, print error 13.11. */
+					if(system_call_status == SYSTEM_CALL_ERROR) {
+						fprintf(stderr, "invalid command\n");
+						fprintf(stderr, "%s failed, errno is %d\n", arguments[0], errno);
+					}
+					exit(0);
+
+				}
+
+				else {
+					sleep(0.1);
+					system_call_status = waitpid(*PID_external_command, (int*)NULL, 0);
+					
+					/* system call fail - print error 13.*/
+					if(system_call_status == -1) {
+						fprintf(stderr,"%s failed, errno is %d\n", arguments[0], errno);
+					}
+				}
 			}
 		}
 
 
 		// reap zombie processes
-		
 		for(int proccess_num = 0; proccess_num < MAX_PROCESSES; proccess_num++) {
+
 			int pid_to_check = pid_array_shared_memory[proccess_num];
 			if(pid_to_check == 0)
 				continue; // skip empty pids
-
-			if(waitpid(pid_to_check, (int*)NULL, WNOHANG)) {
-				printf("%d is dead\n", pid_to_check );
-				pid_array_shared_memory[proccess_num] = 0;
-				(*number_of_active_processes)--;
+			
+			system_call_status = waitpid(pid_to_check, (int*)NULL, WNOHANG);
+			
+			/* system call error print error 13.*/
+			if(system_call_status == -1) {
+				fprintf(stderr,"%s failed, errno is %d\n", arguments[0], errno);
+			}
+			/* system call success - do work*/
+			else {
+				printf("pid %d finished\n", pid_to_check );
+				pid_array_shared_memory[proccess_num] = 0; // remove from shared memory
+				(*number_of_active_processes)--; // reduce active processes counter by one
 			}
 		}
+
 	}
 
 	printf("Exiting...\n");
